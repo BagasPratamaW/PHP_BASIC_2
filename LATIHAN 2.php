7f8b1706fedced8062cb2cb4777ee3f08c1bd204
Bagas P Wibisono <53308520+BagasPratamaW@users.noreply.github.com>
2019-10-09 09:56:45 +0800

    Add files via upload

---
+++ LATIHAN 2.php
@@ -0,0 +1,33 @@
+<?php
+// loooping / pengulangan // 
+// TYPE 1 = FOR//
+//~~~~~~~~~~~~//
+
+// for($i=1; $i <=10; $i++ ){
+//     echo $i . "Bagas<br>";
+// }
+
+// // TYPE 2 = WHILE //
+// $i = 1;
+// while( $i <= 10){
+//     echo $i . "Bagas Pratama <br>";
+//     $i++;
+// }
+
+// TYPE 3 = FOREACH //
+//~~~~~~~~~~~~~~~~~//
+
+// Conditional / pengkondisian // 
+
+$angka = 20;
+if($angka > 10){
+    echo "Lebih Besar Dari 10";
+}elseif ($angka == 10) {
+    echo "Sama DGN 10";
+}
+else {
+    echo " Kurangan dari 10";
+}
+//Buat ARRAY 1 DIMENSI DAN LEBIH DARI 1 DIMENSI//
+//TAMPILKAN DALAM BENTUK PENGULANGAN FOREACH// 
+?>
